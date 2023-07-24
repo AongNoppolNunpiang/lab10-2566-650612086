8272bfe19521c8c713bbb51fa973f8e21f8b8e6a
Aong noppol Nunpiang <nopphol1122@gmail.com>
2023-07-25 05:02:11 +0700

    s

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,24 +1,23 @@
 export const Task = ({
   id,
   title,
+  completed,
   deleteTaskFunc,
   toggleDoneTaskFunc,
-  completed,
 }) => {
   const deleteBtnOnClick = () => {
     deleteTaskFunc(id);
   };
+
   const doneBtnOnClick = () => {
     toggleDoneTaskFunc(id);
   };
 
   return (
     <div className="d-flex p-3 gap-2 align-items-center border-bottom">
-      {completed ? (
-        <span className="text-decoration-line-through">{title}</span>
-      ) : (
-        <span>{title}</span>
-      )}
+      <span className={completed ? "text-decoration-line-through" : ""}>
+        {title}
+      </span>
       <button className="btn btn-success" onClick={doneBtnOnClick}>
         Done
       </button>
